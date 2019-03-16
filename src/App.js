80941f5bfd44f563586168717eab9efd7d177a7b
Luca Koval <lucakoval@me.com>
2019-03-16 18:00:49 -0400

    Creating alerts

--- src/App.js
+++ src/App.js
@@ -8,24 +8,34 @@ import { GROUND_SERVER_URL } from './constants/links'
 import { AUTH_TOKEN_ID } from './constants/constants'
 import Header from './components/header.js'
 
+import gimbalOperations from './operations/gimbalOperations.js'
 
 export class App extends Component {
   constructor(props) {
     super(props)
-    this.state = {
-      mode: 0
-    }
   }
 
   listen(e) {
-    this.state.mode = e.data
+    let mode = -1;
+    if (e.data.localeCompare('retract')) {
+      mode = 0;
+    } else if (e.data.localeCompare('idle')) {
+      mode = 1;
+    } else if (e.data.localeCompare('tracking')) {
+      mode = 2;
+    }
+    // this.props.updateCameraGimbal(mode)
+    let modeObj = { mode: mode }
+    gimbalOperations.updateSettingsStart(modeObj)
+    // console.log(gimbalOperations)
     SnackbarUtil.render('Camera-Gimbal mode has been changed')
   }
 
   componentWillMount() {
-    var eventSourceInitDict = {headers: {'X-AUTH-TOKEN': localStorage.getItem(AUTH_TOKEN_ID)}}
-    var eventSource = new EventSource(GROUND_SERVER_URL + '/api/v1/settings/camera_gimbal/stream', eventSourceInitDict)
-    eventSource.addEventListener('message', this.listen, false)
+    let eventSourceInitDict = {headers: {'X-AUTH-TOKEN': localStorage.getItem(AUTH_TOKEN_ID)}}
+    let eventSource = new EventSource(GROUND_SERVER_URL + '/api/v1/settings/camera_gimbal/stream', eventSourceInitDict)
+    let boundListen = this.listen.bind(this)
+    eventSource.addEventListener('message', boundListen, false)
   }
 
   render() {
@@ -40,4 +50,13 @@ export class App extends Component {
   }
 }
 
+// const mapDispatchToProps = dispatch => ({
+//   updateCameraGimbal: data => gimbalOperations.updateSettingsStart(dispatch),
+// })
+
+// export default connect(null, mapDispatchToProps)(App)
+
+// export App
+
 export default App
+
