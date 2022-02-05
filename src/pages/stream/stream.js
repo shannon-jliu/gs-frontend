98a394beea2d85c1b96b045556489f76a067320a
Jack Williamson <jew256@cornell.edu>
2022-02-05 14:51:49 -0500

    cleaning up

--- src/pages/stream/stream.js
+++ src/pages/stream/stream.js
@@ -1,20 +1,10 @@
-import React, { Component } from 'react'
+import { Component } from 'react'
 import { connect } from 'react-redux'
-
-// import CameraSettings from './cameraSettings.js'
-// import CameraGimbalSettings from './cameraGimbalSettings.js'
-// import GimbalSettings from './gimbalSettings.js'
-
-// import cameraOperations from '../../operations/cameraOperations.js'
-// import cameraGimbalOperations from '../../operations/cameraGimbalOperations.js'
-// import gimbalSettingsOperations from '../../operations/gimbalSettingsOperations.js'
-// import assignmentOperations from '../../operations/assignmentOperations.js'
-
-// import Modes from './components/Modes.js'
+import $ from 'jquery'
 
 import './stream.css'
 
-const NUM_CAMERAS = 26
+const NUM_CAMERAS = 5
 
 export class Stream extends Component {
   constructor() {
@@ -23,15 +13,12 @@ export class Stream extends Component {
       videoScript1Loaded: false,
       videoScript2Loaded: false,
     }
-
-    // this.clearMdlc = this.clearMdlc.bind(this)
   }
 
   componentDidMount() {
     let script = document.createElement('script')
     script.src = 'https://unpkg.com/video.js/dist/video.js'
     script.id = 'videoScript1'
-    // script.innerHTML = 'videojs.Hls.xhr.beforeRequest = function(options) { options.uri = options.uri+"?number=0"; return options;}; '
     document.body.appendChild(script)
 
     document.getElementById('videoScript1').addEventListener('load', () => {
@@ -72,14 +59,18 @@ export class Stream extends Component {
 
   render() {
     const items = []
-
     for (var i = 0; i < NUM_CAMERAS; i++) {
-      var url = "http://localhost:9000/api/v1/stream/playlist?number=" + i
-      items.push(
-        <video-js id="vid1" width="600" height="300" class="vjs-default-skin" controls data-setup='{"fluid": false}'>
-          <source src={url} type="application/x-mpegURL" />
-        </video-js>
-      )
+      var url = 'http://localhost:9000/api/v1/stream/playlist?number=' + i
+      var res = $.ajax({
+        url: url,
+        type: 'GET',
+        async: false
+      })
+      if (res.status == 200)
+        items.push(
+          <video-js id="vid1" width="600" height="300" class="vjs-default-skin" controls data-setup='{"fluid": false}'>
+            <source src={url} type="application/x-mpegURL" />
+          </video-js>)
     }
 
     return (
@@ -89,13 +80,5 @@ export class Stream extends Component {
     )
   }
 }
-
-// const mapDispatchToProps = dispatch => ({
-//   getCameraCapturing: data => cameraOperations.getCapturing(dispatch),
-//   getCameraZoom: data => cameraOperations.getZoom(dispatch),
-//   getCameraGimbalSettings: data => cameraGimbalOperations.getSetting(dispatch),
-//   getGimbalSettingSettings: data => gimbalSettingsOperations.getSetting(dispatch),
-//   clearMdlc: data => assignmentOperations.clearMdlc(dispatch)
-// })
 
 export default connect(null, null)(Stream)
