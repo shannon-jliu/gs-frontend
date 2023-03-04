33401fd08f818fc50b2fa181175039a83fab25a5
Purit <pb465@cornell.edu>
2023-03-04 15:26:15 -0500

    fixed target sighting bug

--- src/pages/settings/fiveTargetsSettings.js
+++ src/pages/settings/fiveTargetsSettings.js
@@ -1,166 +1,166 @@
-import React, { Component } from 'react'
-import { connect } from 'react-redux'
-import PropTypes from 'prop-types'
-import ImmutablePropTypes from 'react-immutable-proptypes'
-import _ from 'lodash'
-import M from 'materialize-css'
-import { fromJS } from 'immutable'
-import fiveTargetsSettingsOperations from '../../operations/fiveTargetsSettingsOperations.js'
-import ShapeSelect from './components/shapeSelect.js'
-import ColorSelect from './components/colorSelect.js'
-import LetterSelect from './components/letterSelect.js'
-import TargetOperations from '../../operations/targetOperations.js'
-import { AUTH_TOKEN_ID } from '../../constants/constants.js'
-import { type } from 'jquery'
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import PropTypes from "prop-types";
+import ImmutablePropTypes from "react-immutable-proptypes";
+import _ from "lodash";
+import M from "materialize-css";
+import { fromJS } from "immutable";
+import fiveTargetsSettingsOperations from "../../operations/fiveTargetsSettingsOperations.js";
+import ShapeSelect from "./components/shapeSelect.js";
+import ColorSelect from "./components/colorSelect.js";
+import LetterSelect from "./components/letterSelect.js";
+import TargetOperations from "../../operations/targetOperations.js";
+import { AUTH_TOKEN_ID } from "../../constants/constants.js";
+import { type } from "jquery";
 
 export class fiveTargetsSettings extends Component {
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
       targets: [
         {
-          shape: '',
-          shapeColor: '',
-          alpha: '',
-          alphaColor: '',
+          shape: "",
+          shapeColor: "",
+          alpha: "",
+          alphaColor: "",
         },
         {
-          shape: '',
-          shapeColor: '',
-          alpha: '',
-          alphaColor: '',
+          shape: "",
+          shapeColor: "",
+          alpha: "",
+          alphaColor: "",
         },
         {
-          shape: '',
-          shapeColor: '',
-          alpha: '',
-          alphaColor: '',
+          shape: "",
+          shapeColor: "",
+          alpha: "",
+          alphaColor: "",
         },
         {
-          shape: '',
-          shapeColor: '',
-          alpha: '',
-          alphaColor: '',
+          shape: "",
+          shapeColor: "",
+          alpha: "",
+          alphaColor: "",
         },
         {
-          shape: '',
-          shapeColor: '',
-          alpha: '',
-          alphaColor: '',
+          shape: "",
+          shapeColor: "",
+          alpha: "",
+          alphaColor: "",
         },
       ],
-    }
-    this.getSavedFields = this.getSavedFields.bind(this)
-    this.getDisplayFields = this.getDisplayFields.bind(this)
-    this.getNewFields = this.getNewFields.bind(this)
-    this.canSave = this.canSave.bind(this)
-    this.save = this.save.bind(this)
-    this.updateTargetInformation = this.updateTargetInformation.bind(this)
+    };
+    this.getSavedFields = this.getSavedFields.bind(this);
+    this.getDisplayFields = this.getDisplayFields.bind(this);
+    this.getNewFields = this.getNewFields.bind(this);
+    this.canSave = this.canSave.bind(this);
+    this.save = this.save.bind(this);
+    this.updateTargetInformation = this.updateTargetInformation.bind(this);
   }
 
   // Used by selector to work to initialize elements
   componentDidMount() {
-    let elems = document.querySelectorAll('select')
-    M.FormSelect.init(elems, {})
+    let elems = document.querySelectorAll("select");
+    M.FormSelect.init(elems, {});
   }
 
   // Checks if any previous and current values are the same.
   // Returns false if any element in the list is the same as it's previous value, and true otherwise.
   compareValues(x) {
-    let check = true
+    let check = true;
     for (var i = 0; i < x.length; i++) {
-      if (x[i].shape === this.state.targets[i].shape) check = false
-      if (x[i].shapeColor === this.state.targets[i].shapeColor) check = false
-      if (x[i].alpha === this.state.targets[i].alpha) check = false
-      if (x[i].alphaColor === this.state.targets[i].alphaColor) check = false
+      if (x[i].shape === this.state.targets[i].shape) check = false;
+      if (x[i].shapeColor === this.state.targets[i].shapeColor) check = false;
+      if (x[i].alpha === this.state.targets[i].alpha) check = false;
+      if (x[i].alphaColor === this.state.targets[i].alphaColor) check = false;
     }
-    return check
+    return check;
   }
 
   // Checks if items updated, and if they all did, sets the state to the updated value.
   componentDidUpdate(prevProps) {
-    let targets = this.props.settings.get('settings').get('targets')
-    if (prevProps.settings.get('settings').get('targets') !== undefined) {
+    let targets = this.props.settings.get("settings").get("targets");
+    if (prevProps.settings.get("settings").get("targets") !== undefined) {
       if (!_.isEqual(prevProps, this.props) && this.compareValues(targets)) {
         this.setState({
           targets: [
             {
-              shape: this.props.settings.get('settings').get('targets')[0]
+              shape: this.props.settings.get("settings").get("targets")[0]
                 .shape,
-              shapeColor: this.props.settings.get('settings').get('targets')[0]
+              shapeColor: this.props.settings.get("settings").get("targets")[0]
                 .shapeColor,
-              alpha: this.props.settings.get('settings').get('targets')[0]
+              alpha: this.props.settings.get("settings").get("targets")[0]
                 .alpha,
-              alphaColor: this.props.settings.get('settings').get('targets')[0]
+              alphaColor: this.props.settings.get("settings").get("targets")[0]
                 .alphaColor,
             },
             {
-              shape: this.props.settings.get('settings').get('targets')[1]
+              shape: this.props.settings.get("settings").get("targets")[1]
                 .shape,
-              shapeColor: this.props.settings.get('settings').get('targets')[1]
+              shapeColor: this.props.settings.get("settings").get("targets")[1]
                 .shapeColor,
-              alpha: this.props.settings.get('settings').get('targets')[1]
+              alpha: this.props.settings.get("settings").get("targets")[1]
                 .alpha,
-              alphaColor: this.props.settings.get('settings').get('targets')[1]
+              alphaColor: this.props.settings.get("settings").get("targets")[1]
                 .alphaColor,
             },
             {
-              shape: this.props.settings.get('settings').get('targets')[2]
+              shape: this.props.settings.get("settings").get("targets")[2]
                 .shape,
-              shapeColor: this.props.settings.get('settings').get('targets')[2]
+              shapeColor: this.props.settings.get("settings").get("targets")[2]
                 .shapeColor,
-              alpha: this.props.settings.get('settings').get('targets')[2]
+              alpha: this.props.settings.get("settings").get("targets")[2]
                 .alpha,
-              alphaColor: this.props.settings.get('settings').get('targets')[2]
+              alphaColor: this.props.settings.get("settings").get("targets")[2]
                 .alphaColor,
             },
             {
-              shape: this.props.settings.get('settings').get('targets')[3]
+              shape: this.props.settings.get("settings").get("targets")[3]
                 .shape,
-              shapeColor: this.props.settings.get('settings').get('targets')[3]
+              shapeColor: this.props.settings.get("settings").get("targets")[3]
                 .shapeColor,
-              alpha: this.props.settings.get('settings').get('targets')[3]
+              alpha: this.props.settings.get("settings").get("targets")[3]
                 .alpha,
-              alphaColor: this.props.settings.get('settings').get('targets')[3]
+              alphaColor: this.props.settings.get("settings").get("targets")[3]
                 .alphaColor,
             },
             {
-              shape: this.props.settings.get('settings').get('targets')[4]
+              shape: this.props.settings.get("settings").get("targets")[4]
                 .shape,
-              shapeColor: this.props.settings.get('settings').get('targets')[4]
+              shapeColor: this.props.settings.get("settings").get("targets")[4]
                 .shapeColor,
-              alpha: this.props.settings.get('settings').get('targets')[4]
+              alpha: this.props.settings.get("settings").get("targets")[4]
                 .alpha,
-              alphaColor: this.props.settings.get('settings').get('targets')[4]
+              alphaColor: this.props.settings.get("settings").get("targets")[4]
                 .alphaColor,
             },
           ],
-        })
+        });
       }
     }
   }
 
   AssignUndefined(x) {
     let targets = [
-      { shape: '', shapeColor: '', alpha: '', alphaColor: '' },
-      { shape: '', shapeColor: '', alpha: '', alphaColor: '' },
-      { shape: '', shapeColor: '', alpha: '', alphaColor: '' },
-      { shape: '', shapeColor: '', alpha: '', alphaColor: '' },
-      { shape: '', shapeColor: '', alpha: '', alphaColor: '' },
-    ]
+      { shape: "", shapeColor: "", alpha: "", alphaColor: "" },
+      { shape: "", shapeColor: "", alpha: "", alphaColor: "" },
+      { shape: "", shapeColor: "", alpha: "", alphaColor: "" },
+      { shape: "", shapeColor: "", alpha: "", alphaColor: "" },
+      { shape: "", shapeColor: "", alpha: "", alphaColor: "" },
+    ];
     for (var i = 0; i < x.length; i++) {
-      if (x[i].shape !== null) targets[i].shape = x[i].shape
-      if (x[i].shapeColor !== null) targets[i].shapeColor = x[i].shapeColor
-      if (x[i].alpha !== null) targets[i].shape = x[i].alpha
-      if (x[i].alphaColor !== null) targets[i].shape = x[i].alphaColor
+      if (x[i].shape !== null) targets[i].shape = x[i].shape;
+      if (x[i].shapeColor !== null) targets[i].shapeColor = x[i].shapeColor;
+      if (x[i].alpha !== null) targets[i].shape = x[i].alpha;
+      if (x[i].alphaColor !== null) targets[i].shape = x[i].alphaColor;
     }
-    return targets
+    return targets;
   }
 
   // Returns saved fields, with any fields that are null just as an empty string (default value)
   getSavedFields() {
-    let newLocal = this.props.settings.get('settings')
-    if (newLocal.get('targets') === undefined) {
+    let newLocal = this.props.settings.get("settings");
+    if (newLocal.get("targets") === undefined) {
       return {
         targets: [
           {
@@ -194,37 +194,37 @@ export class fiveTargetsSettings extends Component {
             alphaColor: null,
           },
         ],
-      }
+      };
     } else {
-      return this.AssignUndefined(newLocal.get('targets'))
+      return this.AssignUndefined(newLocal.get("targets"));
     }
   }
 
   getDisplayFields() {
-    return _.merge(this.getSavedFields(), this.state)
+    return _.merge(this.getSavedFields(), this.state);
   }
 
   getNewFields() {
-    return this.getDisplayFields()
+    return this.getDisplayFields();
   }
 
   // Checks if the settings are able to be saved. Pending is defined in the fiveTargetsReducer as an empty list initially.
   // More than 0 items in 'pending' means system is still saving settings from previous attempt, so can't save right now.
   // Additionally, won't save if current fields are the same as old fields.
   canSave() {
-    let newFields = this.getNewFields()
-    let savedFields = this.getSavedFields()
+    let newFields = this.getNewFields();
+    let savedFields = this.getSavedFields();
     return (
-      this.props.settings.get('pending').size === 0 &&
+      this.props.settings.get("pending").size === 0 &&
       !_.isEqual(newFields, savedFields)
-    )
+    );
   }
 
   // Saves target information, and then resets state back to default values.
   save() {
     if (this.canSave()) {
-      let newFields = this.getNewFields()
-      this.props.updateSettingsStart(newFields)
+      let newFields = this.getNewFields();
+      this.props.updateSettingsStart(newFields);
       // this.setState({
       //   targets: [
       //     {
@@ -260,21 +260,22 @@ export class fiveTargetsSettings extends Component {
       //   ],
       // });
     }
-    this.createTargets()
+    this.createTargets();
   }
 
   updateTargetInformation(evt, target, index, x) {
-    let newState = _.cloneDeep(this.state)
-    let val = evt.target.value
-    if (x == 1) newState.targets[index].shapeColor = val
-    if (x == 2) newState.targets[index].shape = val
-    if (x == 3) newState.targets[index].alphaColor = val
-    if (x == 4) newState.targets[index].alpha = val
-    this.setState(newState)
+    let newState = _.cloneDeep(this.state);
+    let val = evt.target.value;
+    if (x == 1) newState.targets[index].shapeColor = val;
+    if (x == 2) newState.targets[index].shape = val;
+    if (x == 3) newState.targets[index].alphaColor = val;
+    if (x == 4) newState.targets[index].alpha = val;
+    this.setState(newState);
   }
 
   createTargets() {
-    this.props.deleteTargets()
+    //deletes old targets
+    this.props.deleteTargets();
 
     //CREATOR NOT GOING THRU
     const creator = {
@@ -282,30 +283,30 @@ export class fiveTargetsSettings extends Component {
       username: JSON.parse(localStorage.getItem(AUTH_TOKEN_ID)).username,
       address: JSON.parse(localStorage.getItem(AUTH_TOKEN_ID)).address,
       userType: JSON.parse(localStorage.getItem(AUTH_TOKEN_ID)).userType,
-    }
+    };
 
     for (let i = 0; i < this.state.targets.length; i++) {
       const target = {
-        type: 'alphanum',
+        type: "alphanum",
         creator: creator,
         ...this.state.targets[i],
         thumbnailTsid: 0,
         offaxis: false,
-        localId: Math.random() + ':' + Math.random() + ':' + Math.random(),
+        localId: Math.random() + ":" + Math.random() + ":" + Math.random(),
         airdropId: i,
-      }
-      console.log(JSON.stringify(target))
+      };
+      console.log(JSON.stringify(target));
 
       //second param is target sightings
-      this.props.saveTarget(fromJS(target), fromJS([]))
+      this.props.saveTarget(fromJS(target), fromJS([]));
     }
   }
 
   render() {
-    let display = this.getDisplayFields()
+    let display = this.getDisplayFields();
     let saveClass = !this.canSave()
-      ? 'waves-effect waves-light btn grey'
-      : 'waves-effect waves-light btn' // save button
+      ? "waves-effect waves-light btn grey"
+      : "waves-effect waves-light btn"; // save button
 
     // Uses a map to create a selector for the 4 values needed for each of the 5 targets from the props list.
     // Index indicates which target, evt is the event information
@@ -314,7 +315,7 @@ export class fiveTargetsSettings extends Component {
         <div className="card white">
           <div className="card-content">
             <h3>Targets</h3>
-            <br />{' '}
+            <br />{" "}
             {this.state.targets.map((target, index) => {
               return (
                 <>
@@ -326,7 +327,7 @@ export class fiveTargetsSettings extends Component {
                           this.updateTargetInformation(evt, target, index, 1)
                         }
                         value={target.shapeColor}
-                        title={'Shape Color'}
+                        title={"Shape Color"}
                       />
                     </div>
                     <div className="shape">
@@ -335,7 +336,7 @@ export class fiveTargetsSettings extends Component {
                           this.updateTargetInformation(evt, target, index, 2)
                         }
                         value={target.shape}
-                        title={'Shape'}
+                        title={"Shape"}
                       />
                     </div>
                     <div className="alphaColor">
@@ -344,7 +345,7 @@ export class fiveTargetsSettings extends Component {
                           this.updateTargetInformation(evt, target, index, 3)
                         }
                         value={target.alphaColor}
-                        title={'Alpha Color'}
+                        title={"Alpha Color"}
                       />
                     </div>
                     <div className="alpha">
@@ -353,12 +354,12 @@ export class fiveTargetsSettings extends Component {
                           this.updateTargetInformation(evt, target, index, 4)
                         }
                         value={target.alpha}
-                        title={'Alphanumeric'}
+                        title={"Alphanumeric"}
                       />
                     </div>
                   </div>
                 </>
-              )
+              );
             })}
             <div className="obj">
               <button onClick={this.save} className={saveClass}>
@@ -368,7 +369,7 @@ export class fiveTargetsSettings extends Component {
           </div>
         </div>
       </div>
-    )
+    );
   }
 }
 
@@ -377,15 +378,15 @@ const mapStateToProps = (state) => ({
   settings: state.fiveTargetsReducer,
   //calls reducer to get thumbnails
   // thumbnails: getThumbnails(state.thumbnailReducer),
-})
+});
 
 // Redux tool to trigger a state change
 const mapDispatchToProps = (dispatch) => ({
   saveTarget: TargetOperations.saveTarget(dispatch),
   deleteTargets: TargetOperations.deleteAllTargets(dispatch),
-})
+});
 
 export default connect(
   mapStateToProps,
   mapDispatchToProps
-)(fiveTargetsSettings)
+)(fiveTargetsSettings);
--- src/pages/tag/components/TargetSelector.js
+++ src/pages/tag/components/TargetSelector.js
@@ -1,31 +1,48 @@
-import React, { useState, useEffect } from 'react'
-import PropTypes from 'prop-types'
+import React, { useState, useEffect } from "react";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+import { useSelector } from "react-redux";
 
-import { GROUND_SERVER_URL } from '../../../constants/links'
+import { GROUND_SERVER_URL } from "../../../constants/links";
 
-const TargetSelector = ({ getHandler }) => {
-  const [selected, setSelected] = useState(-1)
-  const arr = [0, 1, 2, 3, 4]
+const TargetSelector = ({ setTargetSighting }) => {
+  const [selected, setSelected] = useState(-1);
+  const arr = [0, 1, 2, 3, 4];
+  //this is currently undefined
+  const savedTargets = useSelector((state) => state.targetReducer.get("saved"));
+
+  function saveTargetSighting(id) {
+    const target = savedTargets.find((t) => t.get("airdropId") === id);
+    console.log(JSON.stringify(savedTargets));
+    setTargetSighting(target);
+  }
 
   return (
-    <div style={{ justifyContent: 'left' }}>
+    <div style={{ justifyContent: "left" }}>
       {arr.map((id) => {
         return (
           <button
             style={
               selected === id ? { padding: 10, border: 1 } : { padding: 10 }
             }
-            onClick={() => setSelected(id)}
+            onClick={() => {
+              setSelected(id);
+              saveTargetSighting(id);
+            }}
           >
             <img
-              src={GROUND_SERVER_URL + '/api/v1/thumbnail/' + id}
+              src={GROUND_SERVER_URL + "/api/v1/thumbnail/" + id}
               width="100"
             />
           </button>
-        )
+        );
       })}
     </div>
-  )
-}
+  );
+};
+// const mapStateToProps = (state) => ({
+//   savedTargets: state.targetReducer.get("saved"),
+// });
 
-export default TargetSelector
+// export default connect(mapStateToProps, null)(TargetSelector);
+export default TargetSelector;
